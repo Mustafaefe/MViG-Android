089f519818b462a9939d4de3f613d89d8aa5a3c8
Mustafa EFE <m.efe_1905@hotmail.com>
2015-12-10 22:55:34 +0200

    Added Message Template

--- MViG-Android/app/src/main/java/com/example/mustafa/mvig_android/MainActivity.java
+++ MViG-Android/app/src/main/java/com/example/mustafa/mvig_android/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mustafa.mvig_android;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //mProgress = (ProgressBar) findViewById(R.id.progress_bar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
