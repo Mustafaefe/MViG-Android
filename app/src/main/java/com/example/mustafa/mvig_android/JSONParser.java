59876dfb7853944c3383b1a2d4be0cfac54e6b26
Mustafa EFE <m.efe_1905@hotmail.com>
2016-04-03 22:59:17 +0300

    Added JSONParser file for incoming JSON data

--- app/src/main/java/com/example/mustafa/mvig_android/JSONParser.java
+++ app/src/main/java/com/example/mustafa/mvig_android/JSONParser.java
@@ -4,12 +4,24 @@ package com.example.mustafa.mvig_android;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 
 /**
@@ -17,25 +29,61 @@ import java.net.URL;
  */
 
 
-class JSONParser extends AsyncTask<String, Void, Document> {
+class JSONParser{
 
-    private Exception exception;
+    static InputStream is = null;
+    static JSONArray jObj = null;
+    static String json = "";
 
-       protected Document doInBackground(String... urls) {
-            try {
-                Document doc = Jsoup.connect("http://www.tcmb.gov.tr/kurlar/today.xml").get();
+    // constructor
+    public JSONParser() {
 
-                return doc;
-            } catch (Exception e) {
-                this.exception = e;
-                return null;
+    }
+
+    public JSONArray getJSONFromUrl(String url) {
+
+        // Making HTTP request
+        try {
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "n");
             }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
         }
 
-        protected void onPostExecute(Document feed) {
-            // TODO: check this.exception
-            // TODO: do something with the feed
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONArray(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
         }
+
+        // return JSON String
+        return jObj;
+
+    }
     }
 
 
